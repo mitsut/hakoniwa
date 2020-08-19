f1e1196c9e6a0959efbcee923b526181e554b29b
Mitsutaka Takada <mtakada@nces.i.nagoya-u.ac.jp>
2020-08-19 20:15:44 +0900

    Add build target of athrill and target-rh850f1x.

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 pwd
 
-for repo in athrill-target-ARMv7-A athrill-target-v850e2m 
+for repo in athrill-target-ARMv7-A athrill-target-v850e2m athrill-target-rh850f1x
 do
     cd $repo/build_linux
     make
     cd ../../
     perl ./toppers_utils/makerelease $repo/E_PACKAGE
 done
+
+cd athrill
+perl ../toppers_utils/makerelease
+cp RELEASE/athrill-*.tar.gz
+cd ../
